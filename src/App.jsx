689493e64f016baad210a19dc4232fb93ef0e833
Thomas Tharakan Kuttiyil Oommen <tharakanenator@gmail.com>
2026-06-03 08:08:04 -0400

    Syntax updates for v5

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ export default function App() {
   
   const [isSignUpMode, setIsSignUpMode] = useState(false);
   const [inputUser, setInputUser] = useState('');
-  const [inputPass, setInputPass} = useState('');
+  const [inputPass, setInputPass] = useState('');
   const [authFeedback, setAuthFeedback] = useState('');
 
   // --- Active Workspace Core Decks ---
